774b9abf0580117f8e46cc065fbe12684036e70b
Laura DeCicco <ldecicco@usgs.gov>
2017-04-18 13:48:47 -0500

    Better previous month session

--- scripts/visualize/viz_month_sessions.R
+++ scripts/visualize/viz_month_sessions.R
@@ -5,17 +5,16 @@ visualize.viz_month_sessions <- function(viz = as.viz("viz_month_sessions")){
   
   for(i in unique(viz.data$viewID)){
     sub_data <- filter(viz.data, viewID == i)
-    sessions <- sum(sub_data$sessions, na.rm = TRUE)
+    
     newUsers <- sum(sub_data$newUsers, na.rm = TRUE)
+    sessions <- sum(sub_data$sessions, na.rm = TRUE) - newUsers
+    x <- matrix(c(newUsers, sessions))
+    row.names(x) <- c("New Users","Sessions")
     
-    x <- matrix(c(sessions, newUsers))
-    row.names(x) <- c("Sessions","New Users")
     
-
     png(paste0("cache/visualize/",i,"_session_bar.png"))
-
-    barplot(x, main="Previous Month", horiz=TRUE, 
-              legend=rownames(x), axes=FALSE)
+    par(mar=c(9,1,9,1))
+    barplot(x, horiz=TRUE, axes=FALSE)
     axis(1, lwd = 2, at = pretty(c(0, sum(x))))
     dev.off()
     
