9a9ef2e733c6429670c5222e5ac4d8bd0f3d5b48
Laura DeCicco <ldecicco@usgs.gov>
2017-04-18 14:17:45 -0500

    Starting #7

---
+++ scripts/visualize/visualize.viz_ymd_sessions.R
@@ -0,0 +1,13 @@
+visualize.viz_ymd_sessions <- function(viz = as.viz("viz_ymd_sessions")){
+  library(dplyr)
+  
+  viz.data <- readDepends(viz)[["sessions_and_new_users"]]
+  
+  for(i in unique(viz.data$viewID)){
+
+    
+  }
+  
+
+  
+}
