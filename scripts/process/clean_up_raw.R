a70033eb1697b226e8c9e9dbec7c135fb7ff5901
Laura DeCicco <ldecicco@usgs.gov>
2017-04-18 09:26:12 -0500

    Skeleton to get plots started

--- scripts/process/clean_up_raw.R
+++ scripts/process/clean_up_raw.R
@@ -2,7 +2,7 @@ process.clean_up_raw <- function(viz = as.viz("clean_up_raw")){
   library(dplyr)
   library(data.table)
   
-  viz.data <- readDepends(viz)[["fetchGA"]]
+  viz.data <- readDepends(viz)[["fetchGA_cida"]]
   
   viz.data <- viz.data %>%
     mutate(date = as.Date(paste(year,month,day,sep="-"), origin = "1970-01-01")) %>%
--- scripts/visualize/viz_month_sessions.R
+++ scripts/visualize/viz_month_sessions.R
@@ -3,6 +3,27 @@ visualize.viz_month_sessions <- function(viz = as.viz("viz_month_sessions")){
   
   viz.data <- readDepends(viz)[["sessions_and_new_users"]]
   
+  for(i in unique(viz.data$viewID)){
+    sub_data <- filter(viz.data, viewID == i)
+    sessions <- sum(sub_data$sessions, na.rm = TRUE)
+    newUsers <- sum(sub_data$newUsers, na.rm = TRUE)
+    
+    x <- matrix(c(sessions, newUsers))
+    row.names(x) <- c("Sessions","New Users")
+    
 
+    png(paste0("cache/visualize/",i,"_session_bar.png"))
+
+    barplot(x, main="Previous Month", horiz=TRUE, 
+              legend=rownames(x), axes=FALSE)
+    axis(1, lwd = 2, at = pretty(c(0, sum(x))))
+    dev.off()
+    
+  }
+  
+  write.csv(paste0("cache/visualize/",
+                   unique(viz.data$viewID),
+                   "_session_bar.png"), 
+            file=viz[["location"]], row.names = FALSE)
   
 }
