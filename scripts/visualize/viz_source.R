99d208678b26d8875561ec00a19585492508bd86
Laura DeCicco <ldecicco@usgs.gov>
2017-04-19 11:31:55 -0500

    Source counts

--- scripts/visualize/viz_source.R
+++ scripts/visualize/viz_source.R
@@ -1,7 +1,7 @@
-visualize.viz_device_type <- function(viz = as.viz("viz_device_type")){
+visualize.viz_source <- function(viz = as.viz("viz_source")){
   library(dplyr)
   
-  viz.data <- readDepends(viz)[["device_type"]]
+  viz.data <- readDepends(viz)[["source_counts"]]
   
   x <- data.frame(id = character(),
                   loc = character(),
@@ -9,34 +9,30 @@ visualize.viz_device_type <- function(viz = as.viz("viz_device_type")){
                   stringsAsFactors = FALSE)
 
   plot_type <- viz[["plottype"]]
-  range_text <- viz[["rangetext"]]
-  
-  range_days = seq(Sys.Date(), length = 2, by = range_text)
-  
+
   for(i in unique(viz.data$viewID)){
     sub_data <- filter(viz.data, viewID == i)
     
-    sub_data_range <- sub_data %>%
-      filter(date >= range_days[2]) %>%
-      select(-date) %>%
-      group_by(deviceCategory) %>%
-      summarize(totals = n())
+    source_sum <- data.frame(table(sub_data$source),
+                             stringsAsFactors = FALSE) %>%
+      arrange(desc(Freq))
+    
+    source_sum <- source_sum[1:min(c(5, nrow(source_sum))),]
     
     location <- paste0("cache/visualize/",i,"_",plot_type,".png")
     
-    if(nrow(sub_data_range) > 0){
+    if(nrow(source_sum) > 0){
       
       png(location) 
-      par(oma=c(0,0,0,0),las=1)
-      barplot(sub_data_range$totals, horiz=TRUE,
-              names.arg=sub_data_range$deviceCategory)
+      par(oma=c(0,0,0,0),las=1, mar=c(2,10,2,2))
+      barplot(source_sum$Freq, horiz=TRUE,
+              names.arg=source_sum$Var1)
       dev.off()
     } else {
       png(location) 
-      par(oma=c(0,0,0,0),las=1)
-      barplot(c(0,0,0), horiz=TRUE,
-              names.arg=c("desktop","mobile","tablet"),
-              xlim = c(0,10))    
+      barplot(c(0,0), horiz=TRUE,
+              names.arg=c("google","(direct)"),
+              xlim = c(0,10))         
       dev.off()
     }
     
