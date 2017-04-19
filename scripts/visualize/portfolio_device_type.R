e5a073f474165beab6c8ea73ad39f513916bdbfa
Laura DeCicco <ldecicco@usgs.gov>
2017-04-19 16:41:49 -0500

    Device type for portfolio page

---
+++ scripts/visualize/portfolio_device_type.R
@@ -0,0 +1,33 @@
+visualize.portfolio_device_type <- function(viz = as.viz("portfolio_device_type")){
+  library(dplyr)
+  
+  viz.data <- readDepends(viz)[["device_type"]]
+  
+  range_text <- viz[["rangetext"]]
+  range_days = seq(Sys.Date(), length = 2, by = range_text)
+  
+  sub_data_range <- viz.data %>%
+    filter(date >= range_days[2]) %>%
+    select(-date) %>%
+    group_by(deviceCategory) %>%
+    summarize(totals = n())
+    
+  png(viz[["location"]]) 
+  
+  if(nrow(sub_data_range) > 0){
+    
+    par(oma=c(0,0,0,0),las=1)
+    barplot(sub_data_range$totals, horiz=TRUE,
+            names.arg=sub_data_range$deviceCategory)
+    
+  } else {
+ 
+    par(oma=c(0,0,0,0),las=1)
+    barplot(c(0,0,0), horiz=TRUE,
+            names.arg=c("desktop","mobile","tablet"),
+            xlim = c(0,10))    
+
+  }
+  dev.off()
+
+}
