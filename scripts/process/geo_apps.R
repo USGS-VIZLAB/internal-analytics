669591e5e9441e136a138130a5b95bce1cb5842a
Laura DeCicco <ldecicco@usgs.gov>
2017-04-19 16:16:38 -0500

    Start of maps

--- scripts/process/geo_apps.R
+++ scripts/process/geo_apps.R
@@ -1,16 +1,17 @@
-process.source_counts <- function(viz = as.viz("source_counts")){
+process.geo_apps <- function(viz = as.viz("geo_apps")){
   library(dplyr)
   
   viz.data <- readDepends(viz)[["clean_up_raw"]]
+  
   range_text <- viz[["rangetext"]]
   
   range_days = seq(Sys.Date(), length = 2, by = range_text)
   
-  source_counts <- select(viz.data, date, viewID,source) %>%
+  regions <- select(viz.data, date, viewID, region) %>%
     mutate(date = as.Date(date)) %>%
     filter(date >= range_days[2]) %>%
     select(-date)
   
-  saveRDS(source_counts, file=viz[["location"]])
+  saveRDS(regions, file=viz[["location"]])
   
 }
