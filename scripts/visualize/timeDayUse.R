1368ef769b3dfc89601934ec7088ec36ad03ee28
Laura DeCicco <ldecicco@usgs.gov>
2017-04-20 16:20:30 -0500

    Getting in timeDay

--- scripts/visualize/timeDayUse.R
+++ scripts/visualize/timeDayUse.R
@@ -13,17 +13,32 @@ visualize.timeDayUse_all <- function(viz) {
 }
 
 visualize.timeDayUse_app <- function(viz) {
-  viz.data <- readDepends(viz)[["sessions_and_new_users"]]
+  
+  viz.data <- readDepends(viz)[["aggregate_ga"]]
+  height = viz[["height"]]
+  width = viz[["width"]]
+  plot_type <- viz[["plottype"]]
+  
+  x <- data.frame(id = character(),
+                  loc = character(),
+                  type = character(),
+                  stringsAsFactors = FALSE)
   
   for(i in unique(viz.data$viewID)) {
-    
+    location <- paste0("cache/visualize/timeDayUse_", i, ".png")
     hourSum <- filter(viz.data, viewID == i) %>% group_by(hour) %>% summarise(n = n()) #need to set to numeric?
-    png(paste0("cache/visualize/timeDayUse_", i, ".png"))
+    png(location, height = height, width = width)
     plot(hourSum$hour, hourSum$n, type = "l", xlab = "Hour of Day", 
          ylab = "Sessions", main = "Applications sessions, sum total per hour", 
          xaxt = "n", lwd = 2, col = "blue")
     axis(side = 1, at = 0:23, labels = FALSE)
     axis(side = 1, at = seq(0, 24, by = 4), tck = -0.04)
     dev.off()
+    
+    x <- bind_rows(x, data.frame(id = i,
+                                 loc = location,
+                                 type = plot_type,
+                                 stringsAsFactors = FALSE))    
   }
+  write.csv(x, file=viz[["location"]], row.names = FALSE)
 }
