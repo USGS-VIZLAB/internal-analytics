318ab4a29a9cb2dcf761e38cd98a88d79b7001b6
Laura DeCicco <ldecicco@usgs.gov>
2017-04-20 12:01:17 -0500

    Trying to pretty

--- scripts/visualize/viz_y_sessions.R
+++ scripts/visualize/viz_y_sessions.R
@@ -1,5 +1,6 @@
 visualize.viz_y_sessions <- function(viz){
   library(dplyr)
+  library(ggplot2)
   
   height = viz[["height"]]
   width = viz[["width"]]
@@ -24,31 +25,32 @@ visualize.viz_y_sessions <- function(viz){
       summarize(sessions = sum(sessions, na.rm = TRUE)) 
     
     location <- paste0("cache/visualize/",i,"_",plot_type,".png")
-    png(location, width = width, height = height)
-    
-    if(nrow(sub_data_year) > 0){
-      par(oma=c(0,0,0,0),las=1)
-      plot(x = sub_data_year$date, sub_data_year$sessions, axes=FALSE, 
-           type="l",xlab="",ylab="")
-      axis(1, at=c(0,max(sub_data_year$date)), 
-           labels = c("",""),tck = 0)
-      axis(2, at=c(0,max(sub_data_year$sessions)), 
-           labels = c(0,max(sub_data_year$sessions)),tck = 0)
-      
-    } else {
- 
-      par(oma=c(0,0,0,0),las=1)
-        plot(1, axes=FALSE, 
-             type="n",xlab="",ylab="")
-        axis(1, at=c(0,10), 
-             labels = c("",""),tck = 0)
-        axis(2, at=c(0,10), 
-             labels = c(0,10),tck = 0)      
 
-    }
-    
-    dev.off()
+    if(nrow(sub_data_year) > 0){
       
+      gplot <- ggplot(data = sub_data_year, aes(x=date, y=sessions))
+      
+      if(range_text == "-1 week"){
+        gplot <- gplot +
+          geom_line(aes(x=date, y=sessions)) +
+          geom_point() + geom_line()
+      } else {
+        gplot <- gplot +
+          geom_line()
+      }
+
+    } else {
+      gplot <- ggplot(data = data.frame(date= 1, sessions = 1)) 
+    }
+      
+    gplot <- gplot +
+      theme_minimal() +
+      ylab("") + xlab("") +
+      labs(title = paste(range(sub_data_year$date), collapse = " to ")) +
+      theme(plot.title = element_text(size=7))
+    
+    ggsave(location, width = width, height = height, dpi = 72)
+    
     x <- bind_rows(x, data.frame(id = i,
                                  loc = location,
                                  type = plot_type,
