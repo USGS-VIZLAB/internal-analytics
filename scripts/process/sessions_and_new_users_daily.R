06456bf002b9c36bdf22e2de9ad71d6b4e1d4ba8
wdwatkins <wwatkins@usgs.gov>
2017-04-20 09:16:49 -0500

    dateTime doesn't exist yet

--- scripts/process/sessions_and_new_users_daily.R
+++ scripts/process/sessions_and_new_users_daily.R
@@ -1,16 +1,17 @@
 process.sessions_and_new_users_daily <- function(viz = as.viz("sessions_and_new_users_daily")){
   library(dplyr)
   
-  viz.data <- readDepends(viz)[["clean_up_raw"]]
+  viz.data <- readDepends(viz)[["aggregate_ga"]]
   
   yesterday <- seq(Sys.Date()-1, length = 2, by = "-1 day")[1]
   the_day_before <- seq(Sys.Date()-1, length = 2, by = "-1 day")[2]
   
-  session_users <- select(viz.data, date, dateTime,viewID,sessions,newUsers) %>%
+  session_users <- select(viz.data, date, hour, viewID,sessions,newUsers) %>%
     mutate(date = as.Date(date)) %>%
-    filter(dateTime > the_day_before) %>%
-    filter(dateTime <= yesterday) %>%
-    mutate(dateTime = as.POSIXct(dateTime, format = "%Y-%m-%dT%H:%M:%SZ")) %>%
+    filter(date > the_day_before) %>%
+    filter(date <= yesterday) %>%
+    mutate(dateTime = as.POSIXct(paste0(date, "T", hour, ":00:00"), 
+                                 format = "%Y-%m-%dT%H:%M:%SZ")) %>%
     select(-date)
 
   
