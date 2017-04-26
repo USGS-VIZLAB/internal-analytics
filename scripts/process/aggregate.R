321b96b4c5f4eb40b8e72edf15d9a8f623042505
Jordan Walker <jiwalker@usgs.gov>
2017-04-26 11:00:21 -0500

    free up memory after viz.data is read

--- scripts/process/aggregate.R
+++ scripts/process/aggregate.R
@@ -4,11 +4,12 @@ process.aggregate_ga <- function(viz) {
   library(lubridate)
   viz.data <- readDepends(viz) #list of all depends
   allDataDF <- do.call("bind_rows", viz.data)
-  
+  rm(viz.data) # free memory
+
   #drop data before longer than a year ago
-  allDataDF <- allDataDF %>% mutate(date = as.Date(date)) %>% 
+  allDataDF <- allDataDF %>% mutate(date = as.Date(date)) %>%
               filter(date > (max(date) - duration(1, "year")))
-  
+
   #add dateTime
   saveRDS(object = allDataDF, file=viz[["location"]])
 }
