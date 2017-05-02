06b9a5d252b27aef5596634e785d12b1ace973fd
wdwatkins <wwatkins@usgs.gov>
2017-05-02 16:58:39 -0500

    make sure don't grab today's data

--- scripts/fetch/throwAway_nwis.R
+++ scripts/fetch/throwAway_nwis.R
@@ -1,21 +1,25 @@
 #one-off to chunk the big NWIS call in case it fails midway
 
-starts <- seq(ymd('2011-01-01'), ymd("2017-01-01"), by = "years")
-ends <- seq(ymd('2012-01-01'), ymd("2018-01-01"), by = "years")
 library(dplyr)
 library(googleAnalyticsR)
 library(data.table)
+library(googleAuthR)
+library(lubridate)
+starts <- seq(ymd('2009-01-01'), ymd("2017-01-01"), by = "years")
+ends <- c(seq(ymd('2010-01-01'), ymd('2017-01-01'), by = "years"), Sys.Date() -1)
+
+gar_auth_service('~/.vizlab/VIZLAB-a48f4107248c.json')
 new_GA_DF <- data.frame()
+view <- "18857756"
 for(i in 1:length(starts)) {
-  ga_auth()
   dateRange = c(starts[i], ends[i])
-  idDF <- google_analytics_4(viewId ="49785472", date_range = dateRange, 
+  idDF <- google_analytics_4(viewId =view, date_range = dateRange, 
                              metrics = c("sessions", "users", 'newUsers', 'avgSessionDuration'), 
                              dimensions = c("year","month", "day", "hour",
                                             "deviceCategory", 'region', 'source'), 
                              max = -1, anti_sample = TRUE)
-  idDF <- mutate(idDF, viewID = "49785472")
+  idDF <- mutate(idDF, viewID = view)
   new_GA_DF <- bind_rows(new_GA_DF, idDF)
-  fwrite(x = new_GA_DF, file = "nwisChunk.csv", 
-         quote = TRUE, row.names = FALSE)
+  fwrite(x = new_GA_DF, file = "wwChunk.csv", 
+         quote = TRUE)
 }
--- scripts/process/aggregate.R
+++ scripts/process/aggregate.R
@@ -2,6 +2,7 @@ process.aggregate_ga <- function(viz) {
   library(dplyr)
   library(data.table)
   library(lubridate)
+  library(assertthat)
   viz.data <- readDepends(viz) #list of all depends
   allDataDF <- do.call("bind_rows", viz.data)
   rm(viz.data) # free memory
@@ -11,7 +12,10 @@ process.aggregate_ga <- function(viz) {
     mutate(date = as.Date(date)) %>%
     filter(date > (max(date) - duration(1, "year"))) %>%
     distinct()
-
+  
+  #make sure none of today's data snuck in on one-off GA pull
+  assert_that(max(allDataDF$date) == (Sys.Date() - 1))
+  
   #add dateTime
   saveRDS(object = allDataDF, file=viz[["location"]], compress = FALSE)
 }
