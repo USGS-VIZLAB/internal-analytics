02245e4995a79a51528360895fbf6d4e5138ca5b
wdwatkins <wwatkins@usgs.gov>
2017-05-05 16:37:14 -0500

    check for duplicates!

--- scripts/process/aggregate.R
+++ scripts/process/aggregate.R
@@ -14,6 +14,9 @@ process.aggregate_ga <- function(viz) {
   
   #make sure none of today's data snuck in on one-off GA pull
   assert_that(max(allDataDF$date) == (Sys.Date() - 1))
+  assert_that(!anyDuplicated(allDataDF))
+  #make sure there aren't duplicates!
+
   
   #add dateTime
   saveRDS(object = allDataDF, file=viz[["location"]], compress = FALSE)
