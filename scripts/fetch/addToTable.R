71128bfcb387132168395e24b66cefd924808a9f
wdwatkins <wwatkins@usgs.gov>
2017-05-18 11:42:36 -0500

    don't wanna source this

--- scripts/fetch/addToTable.R
+++ scripts/fetch/addToTable.R
@@ -1,7 +1,17 @@
 library(yaml)
 library(dplyr)
-#write dataframe to yaml
-cat(as.yaml(masterTable, column.major = FALSE), file = "data/gaTable.yaml")
+
 
 #load yaml, convert to data frame
-df <- do.call(what = bind_rows, args = yaml.load_file('data/gaTable.yaml'))
+#commenting out for safety
+#df <- do.call(what = bind_rows, args = yaml.load_file('data/gaTable.yaml'))
+
+#remove cols
+newTable <- select(new, -kind, -selfLink, -currency, -effective,
+                      -created, -updated, -eCommerceTracking, -contains(".href"),
+                      -contains('Query'), -contains('type'), -defaultPage) %>% 
+                rename(viewID = id, viewName = name)
+masterTable <- bind_rows(df, newTable)
+
+#write dataframe to yaml
+cat(as.yaml(masterTable, column.major = FALSE), file = "data/gaTable.yaml")
