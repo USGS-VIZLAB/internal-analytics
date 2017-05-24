35e85e176ea08f781ea68f8e02ccc241e8e21d88
wdwatkins <wwatkins@usgs.gov>
2017-05-24 16:09:36 -0500

    adding new apps

--- scripts/fetch/addToTable.R
+++ scripts/fetch/addToTable.R
@@ -1,17 +1,34 @@
 library(yaml)
 library(dplyr)
-
+library(googleAnalyticsR)
+library(googleAuthR)
 
 #load yaml, convert to data frame
 #commenting out for safety
-#df <- do.call(what = bind_rows, args = yaml.load_file('data/gaTable.yaml'))
+df <- do.call(what = bind_rows, args = yaml.load_file('data/gaTable.yaml'))
+
+gar_auth_service('~/.vizlab/VIZLAB-a48f4107248c.json')
+
+#get new rows to add out of account list
+new <- accSub
+newViewDF <- data.frame()
+for(i in 1:nrow(new)){
+  row <- ga_view(accountId = new$accountId[i], webPropertyId = new$webPropertyId[i],
+                 profileId = new$viewId[i])
+  newViewDF <- bind_rows(newViewDF, data.frame(row, stringsAsFactors = FALSE)) 
+}
+newViewDF <- rename(newViewDF, viewId = id) %>% 
+            mutate(botFilteringEnabled = as.character(botFilteringEnabled))
+newViewDF <- left_join(newViewDF, select(new, viewId, accountName), by = "viewId")
 
 #remove cols
-newTable <- select(new, -kind, -selfLink, -currency, -effective,
-                      -created, -updated, -eCommerceTracking, -contains(".href"),
-                      -contains('Query'), -contains('type'), -defaultPage) %>% 
-                rename(viewID = id, viewName = name)
+newTable <- select(newViewDF, -kind, -selfLink, -currency, -effective,
+                      -created, -updated, -eCommerceTracking, 
+                   -matches('.href|Query|type')) %>% 
+                rename(viewID = viewId, viewName = name) 
 masterTable <- bind_rows(df, newTable)
 
-#write dataframe to yaml
-cat(as.yaml(masterTable, column.major = FALSE), file = "data/gaTable.yaml")
+#write dataframe to yaml, with newlines between sections
+cat(gsub(x = as.yaml(masterTable, column.major = FALSE, indent = 2), 
+         pattern = "- longName", replacement = "\n- longName"),
+    file = "data/gaTable.yaml")
