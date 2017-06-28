4b0bfd15f9a19ce85d1f6a8d31e08a4194c19f90
Laura DeCicco <ldecicco@usgs.gov>
2017-06-28 16:08:26 -0500

    Pad to yesterday

--- scripts/visualize/portfolio_timeline.R
+++ scripts/visualize/portfolio_timeline.R
@@ -1,19 +1,19 @@
 visualize.portfolio_timeline <- function(viz){
   library(dplyr)
   library(ggplot2)
-  
+
   height = viz[["height"]]
   width = viz[["width"]]
   range_text <- viz[["range_text"]]
-  
+
   viz.data <- readDepends(viz)[["viz_data"]]
 
   plot_type <- viz[["plottype"]]
-  
+
   png(viz[["location"]], height = height, width = width, res = 150)
-  
+
   plot_timeline(viz.data, range_text)
-  
+
   dev.off()
 
 }
@@ -21,83 +21,83 @@ visualize.portfolio_timeline <- function(viz){
 visualize.viz_y_sessions <- function(viz){
   library(dplyr)
   library(ggplot2)
-  
+
   height = viz[["height"]]
   width = viz[["width"]]
   range_text = viz[["range_text"]]
-  
+
   viz.data <- readDepends(viz)[["viz_data"]]
-  
+
   x <- data.frame(id = character(),
                   loc = character(),
                   type = character(),
                   stringsAsFactors = FALSE)
 
   plot_type <- viz[["plottype"]]
-  
+
   for(i in unique(viz.data$viewID)){
     sub_data <- filter(viz.data, viewID == i)
-    
+
     location <- paste0("cache/visualize/",i,"_",plot_type,".png")
-    
+
     png(location, height = height, width = width, res = 150)
       plot_timeline(sub_data, range_text)
     dev.off()
-    
+
     x <- bind_rows(x, data.frame(id = i,
                                  loc = location,
                                  type = plot_type,
-                                 stringsAsFactors = FALSE))      
-    
+                                 stringsAsFactors = FALSE))
+
   }
-  
+
   write.csv(x, file=viz[["location"]], row.names = FALSE)
-  
+
 }
 
 plot_timeline <- function(viz.data, range_text){
-  
-  max_date <- max(viz.data$date, na.rm = TRUE)
-  year_days = seq(max_date, length = 2, by = range_text) 
+
+  max_date <- Sys.Date() - 1
+  year_days = seq(max_date, length = 2, by = range_text)
   year_days[1] <- year_days[1] - 1
   full_dates <- seq(max_date, length = -as.numeric(diff(year_days))+1, by=-1)
   empty_df <- data.frame(date = full_dates)
-  
+
   sub_data <- select(viz.data, date, sessions) %>%
     group_by(date) %>%
     summarize(sessions = sum(sessions, na.rm = TRUE)) %>%
     data.frame() %>%
     right_join(empty_df, by="date") %>%
     arrange(desc(date))
-  
+
   sub_data$sessions[is.na(sub_data$sessions)] <- 0
-  
+
   #TODO: add check for nrows == 0
-  
+
   par(oma=c(0,0,0,0),
       mar=c(1.5,2.5,1,1.5),
-      las=1, 
+      las=1,
       mgp = c(1,0.3,0),
       tck=0.02)
-  
+
   type <- ifelse(range_text == "-1 week", "b", "l")
-  
-  plot(x = sub_data$date, 
-       sub_data$sessions, 
+
+  plot(x = sub_data$date,
+       sub_data$sessions,
        type=type,
-       xlab="",ylab="", yaxt='n', 
+       xlab="",ylab="", yaxt='n',
        ylim = c(0, max(sub_data$sessions, na.rm = TRUE)),
        frame.plot = FALSE)
-  
-  last.tick <- tail(pretty(c(0, max(sub_data$sessions, na.rm = TRUE))),2)[1]  
-  axis(1, at=c(par()$usr[1],par()$usr[2]), 
+
+  last.tick <- tail(pretty(c(0, max(sub_data$sessions, na.rm = TRUE))),2)[1]
+  axis(1, at=c(par()$usr[1],par()$usr[2]),
        labels = c("",""), lwd.tick=0)
-  axis(2, at=c(-last.tick, 0, last.tick, last.tick*2), 
+  axis(2, at=c(-last.tick, 0, last.tick, last.tick*2),
        labels = c("","0", pretty_num(last.tick), ""))
   par(xpd = NA)
-  text(par('usr')[1], par('usr')[4]*1.04, 
+  text(par('usr')[1], par('usr')[4]*1.04,
        labels = paste0(range(sub_data$date), collapse = " to "), pos = 4)
-  
+
 }
 
 pretty_num <- function(n){
