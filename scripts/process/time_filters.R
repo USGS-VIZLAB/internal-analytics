96bac1b0fb778747f23548ef5226b258e9488546
Laura DeCicco <ldecicco@usgs.gov>
2017-07-11 11:27:28 -0500

    That was just sillyness, now cleaner

--- scripts/process/time_filters.R
+++ scripts/process/time_filters.R
@@ -1,31 +1,21 @@
-process.month_filter <- function(viz = as.viz("month_filter")){
+process.time_filter <- function(viz = as.viz("month_filter")){
   library(dplyr)
-  
+
   viz.data <- readDepends(viz)[["year_filter"]]
-  
-  range_text <- "-1 month"
-  
+
+  range_text <- viz[["range_text"]]
+
   range_days = seq(max(viz.data$date, na.rm = TRUE), length = 2, by = range_text)
-  
+
   month_data <- viz.data %>%
-    filter(date >= range_days[2]) 
-  
+    filter(date >= range_days[2])
+
   saveRDS(month_data, file=viz[["location"]], compress = FALSE)
-  
+
 }
 
-process.week_filter <- function(viz = as.viz("week_filter")){
-  library(dplyr)
-  
-  viz.data <- readDepends(viz)[["year_filter"]]
-  
-  range_text <- "-1 week"
-  
-  range_days = seq(max(viz.data$date, na.rm = TRUE), length = 2, by = range_text)
-  
-  week_data <- viz.data %>%
-    filter(date >= range_days[2]) 
-  
-  saveRDS(week_data, file=viz[["location"]], compress = FALSE)
-  
+
+
+process.fill_missing <- function(viz = "fill_missing"){
+
 }
