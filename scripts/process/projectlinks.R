c51e60c40295e3f431e723224108b111b1bcbdee
wdwatkins <wwatkins@usgs.gov>
2019-10-07 09:00:08 -0700

    pass main plot bin into projectlinks

--- scripts/process/projectlinks.R
+++ scripts/process/projectlinks.R
@@ -5,11 +5,12 @@ process.projectlinks <- function(viz = as.viz("project_links")) {
   masterTable <- deps[['project_table']]
   sessions_all <- deps[['order_projects']] %>%
     filter(grepl(pattern = "Year", x = type))
+  browser()
   table <- masterTable %>%
      left_join(sessions_all) %>%
     mutate(url = paste0(shortName, ".html")) %>%
     arrange(desc(sessions)) %>%
-    select(shortName, longName, url)
-  links <- apply(table, 1, function(x){list(longName=x[[2]], url=x[[3]])})
+    select(shortName, longName, url, bin)
+  links <- apply(table, 1, function(x){list(longName=x[[2]], url=x[[3]], bin=x[[4]])})
   saveRDS(links, file = viz[['location']])
 }
